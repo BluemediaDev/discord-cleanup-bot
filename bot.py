b86491174478c63a6c14197e79878221f304a7a0
BluemediaGER <oliver@traber-info.de>
2023-07-11 23:02:35 +0200

    Don't delete interaction of purge command

--- bot.py
+++ bot.py
@@ -147,7 +147,7 @@ async def purge(interaction: discord.Interaction, days: int):
     if days > 13:
         await interaction.response.send_message(content='❌ Error: Due to technical limitations, 13 days is the maximum after which I can still delete messages. Please use `13` days or less.', delete_after=20)
         return
-    await purge_messages(channel=interaction.channel, audit_message=f'Deleted via /purge command from user {interaction.user.name}', before=datetime.now(), after=(datetime.now() - timedelta(days=days)))
+    await purge_messages(channel=interaction.channel, audit_message=f'Deleted via /purge command from user {interaction.user.name}', before=interaction.created_at, after=(datetime.now() - timedelta(days=days)))
     await interaction.response.send_message(content='✅ Messages deleted.', delete_after=20)
 
 
